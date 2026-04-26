976bcf567a6074ad461ecaeab044f302ca265137
GyanaprakasKhandual <gyanaprakashkhnadual@gmail.com>
2026-04-26 12:32:57 +0530

    All the uncesary ui compotnen remove from this project

--- src/app/components/utils/Provider.tsx
+++ src/app/components/utils/Provider.tsx
@@ -1,71 +1,50 @@
 "use client";
-
-import { AuthProvider } from "@/app/context/Auth.context";
 import { InputProvider } from "@/ui/inputs/inputs/Input.context";
-import { CardProvider } from "@/ui/data/card/Card.context";
 import { ButtonProvider } from "@/ui/inputs/button/Button.context";
 import { SidebarProvider } from "@/ui/navigations/sidebar/Sidebar.context";
 import AuthenticatedLayout from "@/app/layouts/Authenticate.layout";
-import { TabsProvider } from "@/ui/navigations/tab/Tab.context";
-import { ToggleProvider } from "@/ui/inputs/toggle/Toogle.context";
 import { ThemeProvider } from "@/app/context/Theme.context";
 import { ActionMenuProvider } from "@/ui/navigations/action/Action.menu.context";
-import { NavbarProvider } from "@/ui/navigations/navbar/Navbar.context";
-import { ListProvider } from "@/ui/data/list/List.context";
-import { TableProvider } from "@/ui/data/table/Table.context";
-import { Provider } from "react-redux";
-import { store } from "../../lib/store";
 import { SkeletonProvider } from "@/ui/feedback/skeleton/Skeleton.context";
 import { ConfirmProvider } from "@/ui/overlay/confirm/Confirm.context";
 import { DateTimeProvider } from "@/ui/inputs/time/Time.context";
-import { WindowProvider } from "@/ui/overlay/window/Window.context";
-
 import { AlertProvider } from "@/ui/feedback/alert/Alert.context";
 import { SnackbarProvider } from "@/ui/feedback/snackbar/Snackbar.context";
+
+import { AuthProvider } from "@/app/context/Auth.context";
 import { OnboardProvider } from "@/app/context/Onboard.context";
+
+import { Provider } from "react-redux";
+import { store } from "../../lib/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
       <SnackbarProvider>
         <AlertProvider>
-          <WindowProvider>
-            <DateTimeProvider>
-              <ConfirmProvider>
-                <SkeletonProvider>
-                  <TableProvider children={undefined} columns={[]} data={[]}>
-                    <ListProvider children={undefined} items={[]}>
-                      <NavbarProvider>
-                        <ActionMenuProvider>
-                          <ToggleProvider>
-                            <TabsProvider children={undefined} defaultActiveId={""}>
-                              <SidebarProvider>
-                                <ButtonProvider>
-                                  <InputProvider>
-                                    <CardProvider>
-                                      <OnboardProvider>
-                                      <AuthProvider>
-                                        <ThemeProvider>
-                                          <AuthenticatedLayout>
-                                          {children}
-                                          </AuthenticatedLayout>
-                                        </ThemeProvider>
-                                      </AuthProvider>
-                                      </OnboardProvider>
-                                    </CardProvider>
-                                  </InputProvider>
-                                </ButtonProvider>
-                              </SidebarProvider>
-                            </TabsProvider>
-                          </ToggleProvider>
-                        </ActionMenuProvider>
-                      </NavbarProvider>
-                    </ListProvider>
-                  </TableProvider>
-                </SkeletonProvider>
-              </ConfirmProvider>
-            </DateTimeProvider>
-          </WindowProvider>
+          <DateTimeProvider>
+            <ConfirmProvider>
+              <SkeletonProvider>
+                <ActionMenuProvider>
+                  <SidebarProvider>
+                    <ButtonProvider>
+                      <InputProvider>
+                        <OnboardProvider>
+                          <AuthProvider>
+                            <ThemeProvider>
+                              <AuthenticatedLayout>
+                                {children}
+                              </AuthenticatedLayout>
+                            </ThemeProvider>
+                          </AuthProvider>
+                        </OnboardProvider>
+                      </InputProvider>
+                    </ButtonProvider>
+                  </SidebarProvider>
+                </ActionMenuProvider>
+              </SkeletonProvider>
+            </ConfirmProvider>
+          </DateTimeProvider>
         </AlertProvider>
       </SnackbarProvider>
     </Provider>
